a183d88ebba09655f523c489b8103cb0c49b27d4
Anastasiya Arduganova <AYArduganova@stud.kpfu.ru>
2023-07-05 13:47:07 +0300

    added menu and started work with notes fragment

--- app/src/main/java/com/project/edusync/MainActivity.kt
+++ app/src/main/java/com/project/edusync/MainActivity.kt
@@ -2,11 +2,21 @@ package com.project.edusync
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.project.edusync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val newBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = newBinding
+        setContentView(newBinding.root)
     }
 
 }
---
+++ app/src/main/java/com/project/edusync/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.project.edusync
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.project.edusync.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    private var binding: FragmentMainBinding? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
+        binding?.run {
+            button.setOnClickListener {
+                findNavController().navigate(R.id.action_global_notesFragment2)
+            }
+            button2.setOnClickListener {
+                findNavController().navigate(R.id.action_global_diaryFragment2)
+            }
+            button3.setOnClickListener {
+                findNavController().navigate(R.id.action_global_homeworksFragment2)
+            }
+            button4.setOnClickListener {
+                findNavController().navigate(R.id.action_global_settingsFragment22)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
